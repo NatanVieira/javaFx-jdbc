a32bc497d34fe2d46950364b14bf27a3e43f003e
natanael.vieira <natanaelvieira130@gmail.com>
2019-03-28 20:15:29 -0300

    AboutView

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
@@ -23,15 +22,17 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}*/
+	
+	private static Scene mainScene;
 	@Override
-	public void start(Stage primaryStage) {
+	public synchronized void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
 			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFx application");
 			primaryStage.show();
@@ -39,6 +40,9 @@ public class Main extends Application {
 		catch(IOException e) {
 			e.printStackTrace();
 		}
+	}
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	public static void main(String[] args) {
 		launch(args);
